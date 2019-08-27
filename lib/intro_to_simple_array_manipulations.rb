4b7b2f9ad20d0f7552390cd2487a0aaa7459abad
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-08-27 03:12:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,11 +2,17 @@ def using_push(array, string)
   array.push(string)
 end
 
+
 def using_unshift(array, string)
-  
+  array.unshift(string)
 end
+
 
  def using_unshift(array, string)
   
 end
 
+
+def using_unshift(array, string)
+  
+end
